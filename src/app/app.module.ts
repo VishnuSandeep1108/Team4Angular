709602fe209ac3a061d9f61149eb672d6c4bd46f
suvarna <shsuvarna@deloitte.com>
2024-07-30 18:33:25 +0530

    suvarna shashwath commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardPage1Component } from './card-page1/card-page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardPage1Component
   ],
   imports: [
     BrowserModule
