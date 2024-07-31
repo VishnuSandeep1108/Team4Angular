4a65836fb97393dbe05c8e0e40e2c0b3c93a8c37
VishnuSandeep1108 <mvishnu.1108@gmail.com>
2024-07-31 15:04:50 +0530

    Added Footwear Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FootwearComponent } from './footwear/footwear.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FootwearComponent
   ],
   imports: [
     BrowserModule
